1939e7c6254ddd6a129b8358b2e4fd48f1169108
omid habibi <omid_h28@hotmail.com>
2020-06-28 00:50:19 +0430

    fix examples

--- README.md
+++ README.md
@@ -10,9 +10,12 @@ A Heat Map Calendar based on Github's contributions chart which can be used to v
 ### 1. Depend on it
 Add this to your package's pubspec.yaml file:
 
+Also shamsi_date package is required
+
 ```
 dependencies:
   heatmap_calendar_shamsi: ^1.2.5
+  shamsi_date: ^0.8.1
 ```
 
 ### 2. Install it
@@ -38,35 +41,19 @@ import 'package:heatmap_calendar_shamsi/time_utils.dart';
 
 HeatMapCalendar(
   input: {
-    TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 3))): 5,
-    TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 2))): 35,
-    TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 1))): 14,
-    TimeUtils.removeTime(DateTime.now()): 5,
+    TimeUtils.removeTime(Jalali.now().addDays(-3)): 5,
+    TimeUtils.removeTime(Jalali.now().addDays(-2)): 35,
+    TimeUtils.removeTime(Jalali.now().addDays(-1)): 14,
+    TimeUtils.removeTime(Jalali.now()): 5,
   },
   colorThresholds: {
     1: Colors.green[100],
     10: Colors.green[300],
     30: Colors.green[500]
   },
-  weekDaysLabels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-  monthsLabels: [
-    "",
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ],
-  squareSize: 16.0,
-  textOpacity: 0.3,
-  labelTextColor: Colors.blueGrey,
-  dayTextColor: Colors.blue[500],
-)
+  squareSize: 20.0,
+    textOpacity: 0.3,
+    labelTextColor: Colors.blueGrey,
+    dayTextColor: Colors.blue[500],
+  )
 ```
--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:heatmap_calendar_shamsi/heatmap_calendar_shamsi.dart';
 import 'package:heatmap_calendar_shamsi/time_utils.dart';
+import 'package:shamsi_date/shamsi_date.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,32 +14,16 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: HeatMapCalendar(
             input: {
-              TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 3))): 5,
-              TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 2))): 35,
-              TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 1))): 14,
-              TimeUtils.removeTime(DateTime.now()): 5,
+          TimeUtils.removeTime(Jalali.now().addDays(-3)): 5,
+          TimeUtils.removeTime(Jalali.now().addDays(-2)): 35,
+          TimeUtils.removeTime(Jalali.now().addDays(-1)): 14,
+          TimeUtils.removeTime(Jalali.now()): 5,
             },
             colorThresholds: {
               1: Colors.green[100],
               10: Colors.green[300],
               30: Colors.green[500]
             },
-            weekDaysLabels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-            monthsLabels: [
-              "",
-              "Jan",
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-              "Nov",
-              "Dec",
-            ],
             squareSize: 20.0,
             textOpacity: 0.3,
             labelTextColor: Colors.blueGrey,
